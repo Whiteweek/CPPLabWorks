b1ee622f3daf6fbb2e710468c1f41a219890e63d
Peter Telecaster <60666096+Whiteweek@users.noreply.github.com>
2025-01-17 08:54:53 +0300

    Add files via upload

---
+++ lab3/task.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	double a = 0.1;
+	double b = 1;
+	double h = (b - a) / 10;
+
+	for (double x = a; x <= b; x += h) {
+		double y = (((pow(x, 2) / 4) + (x / 2) + 1) * exp(x / 2));
+		double s = x;
+		int n = 1;
+
+		while (n <= 140) {
+			s += (-1) * pow(x, 2 * n + 1) / tgamma(2 * n + 2);
+			n++;
+		}
+
+		cout << "x = " << x << "; Y(x) = " << y << "; S(x) = " << s << endl;
+	}
+
+	return 0;
+}
