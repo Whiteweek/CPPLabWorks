190dfc1ef03a656d2e5032190a0c700365138862
Peter Telecaster <60666096+Whiteweek@users.noreply.github.com>
2025-01-17 08:56:28 +0300

    Add files via upload

---
+++ lab4/task1.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int n;
+
+	cout << "Введите кол-во элементов в массиве: ";
+	cin >> n;
+
+	if (n <= 0) {
+		cout << "Число элементов должно быть положительным." << endl;
+		return 1;
+	}
+
+	vector<int> a(n);
+
+	cout << "Введите " << n << " элемент(а/ов):" << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	int FNI = -1; // first negative index
+	int LPI = -1; // last positive index
+
+	for (int i = 0; i < n; i++) {
+		if (a[i] < 0 && FNI == -1) {
+			FNI = i;
+		}
+		if (a[i] > 0) {
+			LPI = i;
+		}
+	}
+
+	if (FNI != -1) {
+		cout << "Первый отрицательный элемент: " << a[FNI]
+			<< ", который был введён " << FNI + 1 << "-ым." << endl;
+	}
+	else {
+		cout << "Отрицательных элементов не найдено." << endl;
+	}
+
+	if (LPI != -1) {
+		cout << "Последний положительный элемент: " << a[LPI]
+			<< ", который был введён " << LPI + 1 << "-ым." << endl;
+	}
+	else {
+		cout << "Положительных элементов не обнаружено." << endl;
+	}
+
+	return 0;
+}
---
+++ lab4/task2.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	int n;
+	int k;
+	cout << "Введите количество строк: ";
+	cin >> n;
+	cout << "Введите количество столбцов: ";
+	cin >> k;
+
+	vector<vector<char>> mas(n, vector<char>(k));
+	cout << "Введите элементы массива:\n";
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < k; ++j) {
+			cin >> mas[i][j];
+		}
+	}
+
+	cout << "Символы в углах: ";
+	cout << mas[0][0] << mas[0][k - 1] << mas[n - 1][0] << mas[n - 1][k - 1] << endl;
+
+	return 0;
+}
